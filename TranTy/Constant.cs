4d871e1a72bb6494455a139132019913b971b07c
kudo183 <kudo183@gmail.com>
2016-09-12 13:34:05 +0700

    - change month to combobox.

---
+++ TranTy/Constant.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TranTy
+{
+    public class Constant
+    {
+        public static readonly List<int> Months = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    }
+}
